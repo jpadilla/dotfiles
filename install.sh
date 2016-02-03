8a0476676bf8c65d55d24e76a29014472170c579
José Padilla <jpadilla@webapplicate.com>
2016-02-03 18:40:36 -0400

    Add rvm setup

--- install.sh
+++ install.sh
@@ -88,3 +88,7 @@ pip install flake8
 pyenv local 3.4.3
 pip install --upgrade pip
 pip install flake8
+
+# Install RVM
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby -- --ignore-dotfiles
