4bd941491ec22eb6331176ebe8b32dcf569e7dc9
José Padilla <jpadilla@webapplicate.com>
2018-01-02 16:11:50 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "==> Running bootstrap.sh"
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo "Updating OSX..."
 sudo softwareupdate --install --all --verbose
 
