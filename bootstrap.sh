6a04a0963db3cfe257b09a54aad9d99a9f6075c5
José Padilla <jpadilla@webapplicate.com>
2018-01-02 15:59:41 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,7 @@ echo "Updating OSX..."
 sudo softwareupdate --install --all --verbose
 
 echo "Installing Xcode command line tools..."
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-CMD_LINE_TOOLS=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
-sudo softwareupdate -i "$CMD_LINE_TOOLS" --verbose
-rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+sudo softwareupdate --install "Command Line Tools (macOS High Sierra version 10.13) for Xcode-9.2" --verbose
 
 echo "Cloning dotfiles repo..."
 mkdir -p ~/Projects/Personal
