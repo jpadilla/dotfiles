2716b94a47d83aa769a5c20117f3d178bea13b6a
José Padilla <jpadilla@webapplicate.com>
2018-01-02 14:55:09 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo "==> Running bootstrap.sh"
 
-echo "Updating OSX...."
+echo "Updating OSX..."
 sudo softwareupdate --install --all --verbose
 
 echo "Installing Xcode command line tools..."
-sudo xcode-select --install
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+CMD_LINE_TOOLS=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+sudo softwareupdate -i "$CMD_LINE_TOOLS" --verbose
+rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
-echo "Cloning repo.."
+echo "Cloning dotfiles repo..."
 mkdir -p ~/Projects/Personal
 cd ~/Projects/Personal
 git clone https://github.com/jpadilla/dotfiles.git
