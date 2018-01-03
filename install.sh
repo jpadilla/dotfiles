435fd8a684e44e3e46ab3dd3bde8ba6de347a64d
José Padilla <jpadilla@webapplicate.com>
2018-01-02 19:01:10 -0500

    Use rbenv instead of rvm

--- install.sh
+++ install.sh
@@ -8,43 +8,8 @@ echo "Installing all of Homebrew..."
 brew tap Homebrew/bundle
 brew bundle
 
-# Install nvm
-echo "Installing nvm..."
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | PROFILE=~/.extras bash
-source ~/.bash_profile
-
-# Install latest release of node
-echo "Installing node..."
-nvm install lts/*  --latest-npm
-
-echo "Installing yarn..."
-brew install yarn --without-node
-
-# Install Python
-echo "Installing Python..."
-pyenv install 3.6.3
-pyenv global 3.6.3
-
-# Install Ruby
-echo "Installing Ruby..."
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable --ruby -- --ignore-dotfiles
-
-# VSCode packages
-echo "Installing VSCode Packages..."
-vscode-extensions-update
-
-echo
-echo "==> Done!"
-#!/bin/bash
-echo "==> Running install.sh"
-
-echo "Installing Homebrew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "Installing all of Homebrew..."
-brew tap Homebrew/bundle
-brew bundle
+# Accept Xcode license
+sudo xcodebuild -license accept
 
 # Install nvm
 echo "Installing nvm..."
@@ -65,8 +30,8 @@ pyenv global 3.6.3
 
 # Install Ruby
 echo "Installing Ruby..."
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable --ruby -- --ignore-dotfiles
+rbenv install 2.5.0
+rbenv global 2.5.0
 
 # VSCode packages
 echo "Installing VSCode Packages..."
