7ea36d6664289981eec19a7a96a80420823cfb71
José Padilla <jpadilla@webapplicate.com>
2018-08-01 00:48:52 -0400

    Update OSX preferences

--- osx.sh
+++ osx.sh
@@ -5,8 +5,23 @@ echo "==> Running osx.sh"
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 
-# Disable automatic capitalization as it’s annoying when typing code
+# Disable automatic capitalization
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
 # Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Disable Touch Bar typing suggestions
+defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
+
+# Touch Bar shows expanded control strip
+defaults write com.apple.touchbar.agent PresentationModeGlobal fullControlStrip
